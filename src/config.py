8818c0b025918528fea91bceb5c5567129e02493
hmb104 <hmb104@wiu.edu>
2025-02-16 16:57:19 -0600

    Setup DB config and module

--- src/config.py
+++ src/config.py
@@ -1,3 +1,10 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://hmb104:Hosam!23@hmb104.mysql.pythonanywhere-services.com/flask_lms'
+    DB_USER = os.getenv('DB_USER', 'Educationlife')  # Set user
+    DB_PASSWORD = os.getenv('DB_PASSWORD', 'Hellothere44')  # Set password securely
+    DB_HOST = 'Educationlife.mysql.pythonanywhere-services.com'
+    DB_NAME = 'Educationlife$educationlifeDB'  # Database name
+
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
