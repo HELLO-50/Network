ceb48cd65458dffcf7057ca65114d7cc6316c4af
hmb104 <hmb104@wiu.edu>
2025-02-17 15:29:05 -0600

    Fixed CORS

--- src/app.py
+++ src/app.py
@@ -9,7 +9,8 @@ from routes.student_routes import student_bp
 from routes.admin_routes import admin_bp
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
+CORS(app, resources={r"/*": {"origins": ["*", "https://hello-50.github.io"]}})
 app.config.from_object(Config)
 
 CORS(app, resources={r"/*": {"origins": "*"}})  # 
